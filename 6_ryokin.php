3cbaeee937a85a2e5671372af607e9af82767cab
23jn0116 <23jn0116@jec.ac.jp>
2025-02-06 17:45:21 +0900

    Update 6_ryokin.php

--- 6_ryokin.php
+++ 6_ryokin.php
@@ -1,7 +1,7 @@
 <?php
-require '../helpers/MovieDAO.php';
-require_once '../helpers/KaiinDAO.php';
-require '../helpers/seatDAO.php';
+require './helpers/MovieDAO.php';
+require_once './helpers/KaiinDAO.php';
+require './helpers/seatDAO.php';
 session_start();
 
 // セッションから会員情報を取得
@@ -93,7 +93,7 @@ if(isset($movie))
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <link rel="stylesheet" href="../css/Ryokin.css"> <!-- Ryokin.cssを読み込む -->
+    <link rel="stylesheet" href="./css/Ryokin.css"> <!-- Ryokin.cssを読み込む -->
     <style>
         .ticket-option {
             cursor: pointer;
