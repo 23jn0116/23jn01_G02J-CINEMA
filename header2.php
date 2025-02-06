e3d0efb914c0cf3f5d438b7de016a3a93406d565
23jn0116 <23jn0116@jec.ac.jp>
2025-02-06 17:52:12 +0900

    Update header2.php

--- header2.php
+++ header2.php
@@ -1,13 +1,13 @@
 <header>
     <!-- 外部CSSファイル（HeaderStyle.css）のリンク -->
-    <link href="../css/HeaderStyle.css" rel="stylesheet">
+    <link href="./css/HeaderStyle.css" rel="stylesheet">
 
     <!-- ヘッダーのコンテンツを包含するdiv -->
     <div class="header-content">
         <!-- ロゴ画像をクリックすると、1_1index2.phpに遷移 -->
         <a href="1_1index2.php">
             <!-- ロゴ画像を表示 -->
-            <img src="../images/J-CINEMA.png" alt="Rogo" class="header-rogo">
+            <img src="./images/J-CINEMA.png" alt="Rogo" class="header-rogo">
         </a>
     </div>
 </header>
