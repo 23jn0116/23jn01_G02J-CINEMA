f7a5529e236c0eafff5866c813a3177888e38344
23jn0116 <23jn0116@jec.ac.jp>
2025-02-06 17:50:47 +0900

    Update 9_5Erroryoyaku.php

--- 9_5Erroryoyaku.php
+++ 9_5Erroryoyaku.php
@@ -6,7 +6,7 @@
     <title>予約済み映画</title>
     <!-- BootstrapのCSSをインクルードして、デザインを整える -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="../css/yoyaku.css">
+    <link rel="stylesheet" href="./css/yoyaku.css">
 </head>
 <body>
     <?php include "header.php"; ?>
