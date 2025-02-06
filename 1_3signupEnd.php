54928989b9c66a140c871dbe9bad7a2e40a0fbad
23jn0116 <23jn0116@jec.ac.jp>
2025-02-06 17:41:40 +0900

    Update 1_3signupEnd.php

--- 1_3signupEnd.php
+++ 1_3signupEnd.php
@@ -2,12 +2,12 @@
 <html lang="ja">
 <head>
     <meta charset = "utf-8"> <!-- 文字コードをUTF-8に設定 -->
-    <link href="../css/style.css" rel="stylesheet"> <!-- 外部CSSファイルをリンク -->
+    <link href="./css/style.css" rel="stylesheet"> <!-- 外部CSSファイルをリンク -->
     <title>新規会員登録完了</title> <!-- ページタイトル -->
 </head>
 <body>
     <?php include 'header.php'; ?> <!-- 共通ヘッダーを読み込み -->
-    <link href="../css/style.css" rel="stylesheet"> <!-- 再度外部CSSをリンク -->
+    <link href="./css/style.css" rel="stylesheet"> <!-- 再度外部CSSをリンク -->
     <h1>会員登録が完了しました。</h1> <!-- 会員登録完了メッセージ -->
     
     <!-- ログインページへのリンク -->
