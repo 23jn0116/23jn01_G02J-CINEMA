bb123c331079ab5663f5605e1ec6059f7485ae27
23jn0116 <23jn0116@jec.ac.jp>
2025-02-06 18:14:54 +0900

    Update 9_4kaiinExitEnd.php

--- 9_4kaiinExitEnd.php
+++ 9_4kaiinExitEnd.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>退会手続き完了</title>
-    <link href="./css/style.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
     <style>
         /* 追加のスタイリング */
         body {
